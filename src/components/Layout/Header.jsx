036eab02fa7d789a62a0c01287c7bc40ea5ad34c
Fares Salem <faressalem227@gmail.com>
2024-12-17 11:37:27 +0200

    x

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -5,13 +5,13 @@ import Language from "../../Pages/additions/Language";
 export default function Header() {
   return (
     <header className="flex justify-between items-center px-4 py-2 bg-[#FAFAFA] border-b border-[#00000010]">
-      <div>
+      <div className="w-[250px]">
         <img src={IsisLogo} alt="Logo 1" className="h-10" />
       </div>
-      <div>
+      <div className="w-[250px] flex justify-center">
         <img src={ProjectLogo} alt="Logo 2" className="h-10" />
       </div>
-      <div>
+      <div className="w-[250px] flex justify-end">
         <Language />
       </div>
     </header>
